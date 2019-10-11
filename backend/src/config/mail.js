5870cb2a9fe343b22ea64bffbcac1453b2b84472
rmuliterno <renan-mf1@hotmail.com>
2019-10-11 19:45:53 -0300

    Nodemailer config

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	secure: false,
+	auth: {
+		user: '1b1491add7bd5d',
+		pass: '1f2ed435a13d12',
+	},
+	default: {
+		from: 'Meetapp Team <noreply@meetapp.com>',
+	},
+};
