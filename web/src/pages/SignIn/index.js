22e58d5e0939c92a545fdc639a490482cf7aa935
rmuliterno <renan-mf1@hotmail.com>
2019-10-16 19:31:55 -0300

    First page's styling

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-	return <h1>Login</h1>;
+	function handleSubmit(data) {
+		console.tron.log(data);
+	}
+
+	return (
+		<>
+			<img src={logo} alt="Meetapp" />
+
+			<Form onSubmit={handleSubmit}>
+				<Input name="email" type="email" placeholder="Seu e-mail" />
+				<Input
+					name="password"
+					type="password"
+					placeholder="Sua senha secreta"
+				/>
+
+				<button type="submit">Entrar</button>
+
+				<Link to="/signup">Criar conta grátis</Link>
+			</Form>
+		</>
+	);
 }
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
-	return <div />;
+	return (
+		<>
+			<img src={logo} alt="Meetapp" />
+
+			<form>
+				<input type="text" placeholder="Nome Completo" />
+				<input type="email" placeholder="Seu e-mail" />
+				<input type="password" placeholder="Sua senha secreta" />
+
+				<button type="submit">Criar conta</button>
+
+				<Link to="/">Já tenho login</Link>
+			</form>
+		</>
+	);
 }
