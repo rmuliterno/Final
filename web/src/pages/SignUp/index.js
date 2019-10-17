f403aa277215e201e67158fb056d1699d37f9764
rmuliterno <renan-mf1@hotmail.com>
2019-10-16 21:11:28 -0300

    Adding validations

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+	name: Yup.string().required('O nome é obrigatório'),
+	email: Yup.string()
+		.email('Insira um e-mail válido')
+		.required('O email é obrigatório'),
+	password: Yup.string()
+		.min(6, 'Mínimo seis caracteres')
+		.required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
+	function handleSubmit(data) {
+		console.tron.log(data);
+	}
+
 	return (
 		<>
 			<img src={logo} alt="Meetapp" />
 
-			<form>
-				<input type="text" placeholder="Nome Completo" />
-				<input type="email" placeholder="Seu e-mail" />
-				<input type="password" placeholder="Sua senha secreta" />
+			<Form schema={schema} onSubmit={handleSubmit}>
+				<Input name="name" type="text" placeholder="Nome Completo" />
+				<Input name="email" type="email" placeholder="Seu e-mail" />
+				<Input
+					name="password"
+					type="password"
+					placeholder="Sua senha secreta"
+				/>
 
 				<button type="submit">Criar conta</button>
 
 				<Link to="/">Já tenho login</Link>
-			</form>
+			</Form>
 		</>
 	);
 }
