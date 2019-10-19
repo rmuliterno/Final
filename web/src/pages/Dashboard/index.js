afb7c96c990ee26d2af538df3bce561c200f59eb
rmuliterno <renan-mf1@hotmail.com>
2019-10-19 17:14:42 -0300

    Dashboard styling

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import { MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Header, Schedule } from './styles';
 
 export default function Dashboard() {
-	return <h1>Alooo</h1>;
+	return (
+		<Container>
+			<Header>
+				<strong>Meus Meetups</strong>
+				<button type="button">Novo Meetup</button>
+			</Header>
+
+			<ul>
+				<Schedule>
+					<strong>Meetup de React Native</strong>
+					<div>
+						<strong className="date">24 de Junho, às 20h</strong>
+						<MdChevronRight
+							className="icon-right"
+							size={32}
+							color="#fff"
+						/>
+					</div>
+				</Schedule>
+
+				<Schedule>
+					<strong>Meetup de React Native</strong>
+					<div>
+						<strong className="date">24 de Junho, às 20h</strong>
+						<MdChevronRight
+							className="icon-right"
+							size={32}
+							color="#fff"
+						/>
+					</div>
+				</Schedule>
+
+				<Schedule>
+					<strong>Meetup de React Native</strong>
+					<div>
+						<strong className="date">24 de Junho, às 20h</strong>
+						<MdChevronRight
+							className="icon-right"
+							size={32}
+							color="#fff"
+						/>
+					</div>
+				</Schedule>
+
+				<Schedule>
+					<strong>Meetup de React Native</strong>
+					<div>
+						<strong className="date">24 de Junho, às 20h</strong>
+						<MdChevronRight
+							className="icon-right"
+							size={32}
+							color="#fff"
+						/>
+					</div>
+				</Schedule>
+			</ul>
+		</Container>
+	);
 }
---
+++ web/src/pages/Dashboard/styles.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+	max-width: 1100px;
+	margin: 50px auto;
+
+	display: flex;
+	flex-direction: column;
+`;
+
+export const Header = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 62px;
+
+	strong {
+		color: #ffffff;
+		font-size: 32px;
+		font-weight: 400;
+		line-height: 43px;
+	}
+
+	button {
+		height: 44px;
+		width: 162px;
+		background: #f94d6a;
+		font-weight: bold;
+		color: #fff;
+		border: 0;
+		border-radius: 6px;
+		font-size: 16px;
+		transition: background 0.3s;
+
+		&:hover {
+			background: ${darken(0.05, '#f94d6a')};
+		}
+	}
+`;
+
+export const Schedule = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	background: rgba(0, 0, 0, 0.1);
+	border: 0;
+	border-radius: 6px;
+	height: 62px;
+	padding: 0 15px;
+	color: #fff;
+	margin: 0 0 10px;
+
+	strong {
+		color: #ffffff;
+		font-size: 18px;
+		font-weight: 400;
+	}
+
+	.date {
+		/* line-height: 62px; */
+	}
+
+	.icon-right {
+		margin-left: 24px;
+		font-size: 24px;
+		font-weight: 400;
+		line-height: 62px;
+		padding-top: 5px;
+	}
+`;
