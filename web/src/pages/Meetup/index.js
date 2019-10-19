f9f0e23c9a7e79ed18d880263406fedc20ce6006
rmuliterno <renan-mf1@hotmail.com>
2019-10-18 22:51:23 -0300

    Meetup create/update page

--- web/src/pages/Meetup/index.js
+++ web/src/pages/Meetup/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
-export default function Meetup() {
-	return <div />;
+import { Container } from './styles';
+
+export default function Profile() {
+	const dispatch = useDispatch();
+	const profile = useSelector(state => state.user.profile);
+
+	function handleSubmit(data) {
+		dispatch(updateProfileRequest(data));
+	}
+
+	return (
+		<Container>
+			<Form initialData={profile} onSubmit={handleSubmit}>
+				<Input name="title" placeholder="Título do Meetup" />
+				<Input
+					multiline
+					name="description"
+					placeholder="Descrição completa"
+				/>
+				<Input name="date" placeholder="Data do Meetup" />
+				<Input name="location" placeholder="Localização" />
+
+				<button type="submit">Salvar Meetup</button>
+			</Form>
+		</Container>
+	);
 }
---
+++ web/src/pages/Meetup/styles.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+	max-width: 1100px;
+	margin: 50px auto;
+
+	form {
+		display: flex;
+		flex-direction: column;
+		margin-top: 30px;
+
+		input {
+			background: rgba(0, 0, 0, 0.1);
+			border: 0;
+			border-radius: 6px;
+			height: 44px;
+			padding: 0 15px;
+			color: #fff;
+			margin: 0 0 10px;
+
+			&::placeholder {
+				color: rgba(255, 255, 255, 0.7);
+			}
+		}
+
+		textarea {
+			justify-items: center;
+			background: rgba(0, 0, 0, 0.1);
+			border: 0;
+			border-radius: 6px;
+			height: 200px;
+			resize: none;
+			padding: 0 15px;
+			color: #fff;
+			margin: 0 0 10px;
+			line-height: 24px;
+
+			&::placeholder {
+				color: rgba(255, 255, 255, 0.7);
+				padding-top: 5px;
+				font-size: 14px;
+			}
+		}
+
+		span {
+			color: #f64c75;
+			align-self: flex-start;
+			margin: 0 0 10px;
+			font-weight: bold;
+		}
+
+		button {
+			margin: 5px auto;
+			height: 44px;
+			width: 162px;
+			background: #f94d6a;
+			font-weight: bold;
+			color: #fff;
+			border: 0;
+			border-radius: 6px;
+			font-size: 16px;
+			transition: background 0.3s;
+
+			&:hover {
+				background: ${darken(0.05, '#f94d6a')};
+			}
+		}
+	}
+`;
