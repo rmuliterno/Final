89ca7c75627d1a06af1dc255a0d4cf8a1e159e9f
rmuliterno <renan-mf1@hotmail.com>
2019-10-23 20:31:57 -0300

    Initial route config and Redux connection

--- mobile/src/config/ReactotronConfig.js
+++ mobile/src/config/ReactotronConfig.js
@@ -1,13 +1,15 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
 	const tron = Reactotron.configure({ name: 'MeetApp' })
+		.setAsyncStorageHandler(AsyncStorage)
 		.useReactNative()
 		.use(reactotronRedux())
 		.use(reactotronSaga())
-		.connect({ enabled: true, host: '192.168.99.100', port: '9090' });
+		.connect({ enabled: true, host: '192.168.0.10', port: '9090' });
 
 	tron.clear();
 
