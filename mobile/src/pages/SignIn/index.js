b82958fcf49ada0ccea50d389d68970c118c90df
rmuliterno <renan-mf1@hotmail.com>
2019-10-22 21:28:27 -0300

    Sign in and Sign up pages

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignIn() {
-	return <Text>Signin</Text>;
+import Background from '~/components/Background';
+
+import {
+	Container,
+	Form,
+	FormInput,
+	SubmitButton,
+	SignLink,
+	SignLinkText,
+} from './styles';
+
+export default function SignIn({ navigation }) {
+	return (
+		<Background>
+			<Container>
+				<Image source={logo} />
+
+				<Form>
+					<FormInput
+						icon="mail-outline"
+						keyboardType="email-address"
+						autoCorrect={false}
+						autoCapitalize="none"
+						placeholder="Digite seu e-mail"
+					/>
+					<FormInput
+						icon="lock-outline"
+						secureTextEntry
+						placeholder="Sua senha secreta"
+					/>
+
+					<SubmitButton onPress={() => {}}>Entrar</SubmitButton>
+				</Form>
+
+				<SignLink onPress={() => navigation.navigate('SignUp')}>
+					<SignLinkText>Criar conta grátis</SignLinkText>
+				</SignLink>
+			</Container>
+		</Background>
+	);
 }
