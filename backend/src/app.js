b1f27e954d74bb80d28768aab859d15991e17643
rmuliterno <renan-mf1@hotmail.com>
2019-10-27 11:51:51 -0300

    Adding a rate limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,9 @@ import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import cors from 'cors';
 import helmet from 'helmet';
-
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -34,6 +36,21 @@ class App {
 			'/files',
 			express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
 		);
+
+		if (process.env.NODE_ENV !== 'development') {
+			this.server.use(
+				new RateLimit({
+					store: new RateLimitRedis({
+						client: redis.createClient({
+							host: process.env.REDIS_HOST,
+							port: process.env.REDIS_PORT,
+						}),
+					}),
+					windowMs: 1000 * 60 * 5,
+					max: 150,
+				})
+			);
+		}
 	}
 
 	routes() {
